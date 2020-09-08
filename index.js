8fa428ee0806a109ff3ab8297b96f88a917c4559
Andrés Felipe Carreño <carrenoaf14@gmail.com>
2020-09-08 17:34:45 -0500

    Andrés - change to cors

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ var app = express();
 
 var port = process.env.port || 3000;
 
+var cors = require('cors')
+
 var path = require('path');
 
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname + '/snap.svg.html'));
-});
+app.use(cors())
+app.use('/assets', express.static('assets'))
 
+app.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname + '/snap.svg.html'));
+})
 app.listen(port, function(){
     console.log('server is running on :' + port);
 });
-
-app.use(express.static(__dirname + '/public'));
+ 
