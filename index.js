e7213c147c5e584c2044f8162fd648e4b8afabe1
Daniel Vega <veganievesdaniel1@gmail.com>
2020-09-08 22:50:08 -0500

    svg

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-var express = require('express');
+var express = require("express");
 
 var app = express();
 
 var port = process.env.port || 3000;
 
-var cors = require('cors')
+var cors = require("cors");
 
-var path = require('path');
+var path = require("path");
 
-app.use(cors())
-app.use('/assets', express.static('assets'))
+app.use(cors());
+app.use("/assets", express.static("assets"));
 
-app.get('/', function (req, res, next) {
-  res.sendFile(path.join(__dirname + '/snap.svg.html'));
-})
-app.listen(port, function(){
-    console.log('server is running on :' + port);
+app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, "./dist")));
+
+app.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname + "/snap.svg.html"));
 });
- 
+
+app.listen(port, function () {
+  console.log("server is running on :" + port);
+});
